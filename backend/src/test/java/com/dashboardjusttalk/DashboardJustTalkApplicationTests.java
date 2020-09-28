701a970c70c15dfc9aee6e80ac2fbb159892f7c7
Anthony Bravo <anthony.bravo.berrio@gmail.com>
2020-09-27 23:37:55 -0500

    Se configura test para el trabajo con base de datos en memoria

--- backend/src/test/java/com/dashboardjusttalk/DashboardJustTalkApplicationTests.java
+++ backend/src/test/java/com/dashboardjusttalk/DashboardJustTalkApplicationTests.java
@@ -4,10 +4,12 @@ package com.dashboardjusttalk;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class DashboardJustTalkApplicationTests {
 
 	@Test
