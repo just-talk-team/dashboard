7ff6ddd9fb2cdc4b4d98d0be34f43c5c16bf4841
Anthony Bravo <anthony.bravo.berrio@gmail.com>
2020-09-28 01:37:56 -0500

    Se corrige ubicacion de properties para test

--- backend/src/test/java/com/dashboardjusttalk/DashboardJustTalkApplicationTests.java
+++ backend/src/test/java/com/dashboardjusttalk/DashboardJustTalkApplicationTests.java
@@ -4,12 +4,10 @@ package com.dashboardjusttalk;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
 class DashboardJustTalkApplicationTests {
 
 	@Test
